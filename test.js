c60163d910721b6ea21460afe7887658fb19ae08
RollingWorld <stig1467@gmail.com>
2020-06-14 17:45:24 +0300

    rtmp server added

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const node_media_server = require('./media-server');
 const app = express()
 const mysql = require("mysql2");
 const connection = mysql.createPool({
@@ -9,12 +10,13 @@ const connection = mysql.createPool({
     password: 'outofstyle'
 });
 
-app.get("/", (req, res) => {
-   let sql = 'SELECT * FROM test'
-
-   connection.query(sql, [], (err, results) => {
-       res.send(results);
-   }) ;
-});
+// app.get("/", (req, res) => {
+//    let sql = 'SELECT * FROM test'
+//
+//    connection.query(sql, [], (err, results) => {
+//        res.send(results);
+//    }) ;
+// });
 
 app.listen(3000);
+node_media_server.run();
